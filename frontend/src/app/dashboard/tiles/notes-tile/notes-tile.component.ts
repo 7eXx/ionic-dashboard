91ba2f5d5ff21e503347fa49b9fe9e3dc8747709
7eXx <marco.tex@live.com>
2023-08-27 19:15:39 +0200

    feat: notes tile see how many notes you have. quick insert a new note.

--- frontend/src/app/dashboard/tiles/notes-tile/notes-tile.component.ts
+++ frontend/src/app/dashboard/tiles/notes-tile/notes-tile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Note, NotesService} from "../../../services/notes.service";
+import {Observable, Subscription} from "rxjs";
+import {Router} from "@angular/router";
+import {NoteDetailComponent} from "../../notes/note-detail/note-detail.component";
+import {ModalController, ToastController} from "@ionic/angular";
+import {ToastManager} from "../../../shared/toast-manager.component";
 
 @Component({
   selector: 'app-notes-tile',
@@ -7,10 +13,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesTileComponent  implements OnInit {
 
+  toastManager: ToastManager;
+
   title = 'Notes';
 
-  constructor() { }
+  notes: Note[] = [];
+  notesSubscription!: Subscription;
 
-  ngOnInit() {}
+  constructor(private modalController: ModalController,
+              private toastController: ToastController,
+              private notesService: NotesService,
+              private router: Router) {
+    this.toastManager = new ToastManager(toastController);
+  }
+
+  ngOnInit() {
+    this.subscribeNotes();
+  }
+
+  private subscribeNotes() {
+    this.unsubscribeNotes();
+    this.notesSubscription = this.notesService.getNotes()
+      .subscribe((notes) => this.notes = notes);
+  }
+
+  private unsubscribeNotes() {
+    if (this.notesSubscription) {
+      this.notesSubscription.unsubscribe();
+    }
+  }
+
+  public reload() {
+    this.unsubscribeNotes();
+    this.subscribeNotes();
+  }
+
+  onClickTile() {
+    this.router.navigate(['dashboard', 'notes']);
+  }
+
+  async onAddNote($event: Event) {
+    $event.stopPropagation();
+    const modal = await this.modalController.create({
+      component: NoteDetailComponent,
+    });
+    await modal.present();
+
+    const { data, role } = await modal.onWillDismiss();
+    if (role === 'confirm') {
+      await this.toastManager.showSavedSuccessfully()
+      this.reload();
+    }
+  }
 
 }
